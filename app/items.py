67bcb68aed534dc9f0fe81e9b1b9793fa84d8203
noprom <tyee.noprom@qq.com>
2016-07-11 14:16:50 +0800

    init google play crawer

--- app/items.py
+++ app/items.py
@@ -12,3 +12,7 @@ class AppItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class GoogleItem(scrapy.Item):
+ 	url = scrapy.Field()
+ 	num = scrapy.Field()
--- app/spiders/google_play.py
+++ app/spiders/google_play.py
@@ -1,13 +1,27 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.contrib.linkextractors import LinkExtractor
+from app.items import GoogleItem
 
 class GooglePlaySpider(scrapy.Spider):
     name = "google-play"
     allowed_domains = ["play.google.com"]
     start_urls = (
-        'http://www.play.google.com/',
+        'https://play.google.com/store',
+        'https://play.google.com/store/apps/details?id=com.viber.voip'
     )
 
+    rules = [
+        Rule(LinkExtractor(allow=("https://play\.google\.com/store/apps/details", )), callback='parse',follow=True),
+    ] #  CrawlSpider 会根据 rules 规则爬取页面并调用函数进行处理
+
     def parse(self, response):
-        pass
+        # 获取页面的 URL 以及下载数量
+    	item = GoogleItem()
+    	item['url'] = response.url
+    	item['num'] =  response.xpath("//div[@itemprop='numDownloads']").xpath("text()").extract()
+        print item['url'], '----->', item['num']
+    	yield item
